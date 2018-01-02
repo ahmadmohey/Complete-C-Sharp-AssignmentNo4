5a288d8a7993c56314490fd79aacc4963d0ae017
ahmadmohey <ahmadmohey@gmail.com>
2018-01-02 17:21:07 +0200

    Complete C# AssignmentNo4 Solution

---
+++ AssignmentNo4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentNo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int j = 15; j >= 0; j--)
+            {
+                for (int t = 0; t <= j; t++)
+                {
+                    Console.Write("0 ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
